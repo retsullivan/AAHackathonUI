6838d4425b7a0d3199597f02c8f84c8f0afb790f
Zach Cannon <zach.cannon@improving.com>
2019-12-09 15:03:24 -0600

    Showing worst itienerary

--- src/app/models/flight-data.ts
+++ src/app/models/flight-data.ts
@@ -1,28 +1,28 @@
 import { DatePipe } from '@angular/common';
 
-export class FlightData{
-    index:number;
-    flightNumber:number
+export class FlightData {
+    index: number;
+    flightNumber: number
     origin: string;
-    destination:string;
-    travelTime:number;
+    destination: string;
+    travelTime: number;
     departureTime: Date
     arrivalTime: Date;
 
     constructor(flightData: FlightData) {
         if (flightData) {
-          this.index= flightData.index;
-          this.flightNumber = flightData.flightNumber;
-          this.origin = flightData.origin;
-          this.destination =flightData.destination;
-          this.travelTime = flightData.travelTime;
-          this.departureTime = flightData.departureTime;
-          this.arrivalTime = flightData.arrivalTime;
+            this.index = flightData.index;
+            this.flightNumber = flightData.flightNumber;
+            this.origin = flightData.origin;
+            this.destination = flightData.destination;
+            this.travelTime = flightData.travelTime;
+            this.departureTime = flightData.departureTime;
+            this.arrivalTime = flightData.arrivalTime;
         }
     }
 
-    formatDate(date:Date){
-        return new DatePipe('en-US').transform(date,"YYYY-MM-DD");
+    formatDate(date: Date) {
+        return new DatePipe('en-US').transform(date, "YYYY-MM-DD");
 
     }
 }
